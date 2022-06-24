c3c68fa21cb2283153838c03689ddfb2a55e065f
ADETOYESE MATTHEW <adetoyesematthew97@gmail.com>
2022-06-24 04:03:05 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let myname = 'Adetoyese Samson'
+let height = '175cm'
+let country = 'Nigeria'
+
+console.log(myname);
+console.log(height);
+console.log(country);
+
+alert(myname);
+alert(height);
+alert(country);
